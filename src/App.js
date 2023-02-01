3896991529d4c9b595b6a4e402bdede77c15aefe
Maciej Lukaszewski <mlukaszewski86@gmail.com>
2023-02-01 22:39:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from "./footer";
+import Form from "./form";
+import Currency from "./Currency";
+import Title from "./Title";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <main>
+        <Title description="Kalkulator walut" />
+        <Form />
+        <Currency />
+      </main>
+      <Footer />
     </div>
   );
 }
